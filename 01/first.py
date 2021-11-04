a989b4afa8218a4d055ab826d14a98559fab6e1a
irwxy7 <91718565+irwxy7@users.noreply.github.com>
2021-11-04 11:34:12 +0100

    2. alkalom

--- 01/first.py
+++ 01/first.py
@@ -5,4 +5,4 @@ print("Pls give me your age")
 age = int(input())
 
 print("Your name: {0}, age: {1}".format(name, age))
-print("Next year zou will be {0} years old".format(age + 1)
+print("Next year zou will be {0} years old".format(age + 1))
